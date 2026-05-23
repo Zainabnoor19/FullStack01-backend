c61b45a1897d4ed7487f552c2d96a8f0fea6d9f4
Zainabnoor19 <noorwaniya315@gmail.com>
2026-05-23 13:06:42 +0500

    fix9

--- src/config/db.js
+++ src/config/db.js
@@ -5,9 +5,11 @@ const connectDb = async()=>{
     try {
         console.log('check Strig',process.env.MONGOURI);
         
-         await mongoose.connect(process.env.MONGOURI);
-         console.log('mongo db connected');
-         
+        //  await mongoose.connect(process.env.MONGOURI);
+        //  console.log('mongo db connected');
+        await mongoose.connect(process.env.MONGODB_URI)
+  .then(() => app.listen(port, () => console.log('Server running')))
+  .catch(err => console.error('DB Connection Error:', err));
     } catch (error) {
         console.log('error in db-->',error);
         
